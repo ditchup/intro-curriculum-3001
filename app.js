1ffe57ac13395a51e3d8ef5d21f9af89bd73ff7c
Takuya Mizokami <t.m.timpani@gmail.com>
2016-11-06 23:40:56 +0000

    create factrial function

--- app.js
+++ app.js
@@ -7,7 +7,12 @@
  */
 function factorial(n) {
     let result = 1;
-    // TODO このコメントを消して正しく実装してください。
+    n = n || 1;
+
+    for (let i = 1; i <= n; i++) {
+        result *= i;
+    }
+
     return result;
 }
 
